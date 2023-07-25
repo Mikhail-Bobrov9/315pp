2375f291defca2f49c3eadef3fc2ef7dbb707c16
Mikhail-Bobrov9 <98587633+Mikhail-Bobrov9@users.noreply.github.com>
2023-07-26 01:53:29 +0300

    Add files via upload

---
+++ pp3_1_5/src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,22 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import ru.kata.spring.boot_security.demo.models.User;
+
+import java.util.List;
+
+public interface UserService extends UserDetailsService {
+
+    User findByUsername(String username);
+
+    void save(User user);
+
+    User getUserById(Long id);
+
+    void deleteUser(Long id);
+
+    List<User> getAllUsers();
+
+    void update(User user);
+
+}
